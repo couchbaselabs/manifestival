d152f58a72ba48f580df7fb87678ea3385228b03
Steve Yen <steve.yen@gmail.com>
2012-08-30 14:37:39 -0700

    only consider manifest.xml (fixed)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 function on_index_html(html) {
-    // Look for patterns like:  href="couchbase-server-community_x86_64_2.0.0c-709-rel.rpm"
-    var hrefs = html.match(/href="[^\"]+"/g);
+    // Look for patterns like:  href="couchbase-server-community_x86_64_2.0.0c-709-rel.rpm.manifest.xml"
+    var hrefs = html.match(/href="[^\"]+\.manifest\.xml"/g);
     for (var i = 0; i < hrefs.length; i++) {
         var href = hrefs[i];
         var url = href.split('"')[1];
-        if url.match(/\.manifest\.xml$/) {
-            var parts = url.split('.');
-            console.debug(url, parts);
-        }
+        var parts = url.split('.');
+        console.debug(url, parts);
     }
 }
 
