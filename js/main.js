7c2a284da46f27531e36f0c9dd821ff358229065
Steve Yen <steve.yen@gmail.com>
2012-08-30 14:19:30 -0700

    first regexp

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+function on_index_html(html) {
+    // Look for patterns like:  href="couchbase-server-community_x86_64_2.0.0c-709-rel.rpm"
+    var m = html.match(/href="([.]+)"/g);
+    alert(m);
+}
+
 $(document).ready(function(){
         $.get("../../index.html", null,
               function(response, textStatus, jqXHR){
-                  alert(response);
+                  on_index_html(response);
               });
     });
