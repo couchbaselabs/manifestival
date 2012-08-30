a0eca535d15f46eb64f8c60034f9f5a8dbf9a0eb
Steve Yen <steve.yen@gmail.com>
2012-08-30 14:27:49 -0700

    loop through all hrefs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 function on_index_html(html) {
     // Look for patterns like:  href="couchbase-server-community_x86_64_2.0.0c-709-rel.rpm"
-    var m = html.match(/href="([.]+)"/g);
-    alert(m);
+    var hrefs = html.match(/href="[^\"]+"/g);
+    for (var i = 0; i < hrefs.length; i++) {
+        href = hrefs[i];
+        console.debug(href);
+    }
 }
 
 $(document).ready(function(){
