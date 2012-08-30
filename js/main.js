0419beafaaa9baf425c0e62cea114964ac521d2c
Steve Yen <steve.yen@gmail.com>
2012-08-30 14:35:05 -0700

    only consider manifest.xml links

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ function on_index_html(html) {
     // Look for patterns like:  href="couchbase-server-community_x86_64_2.0.0c-709-rel.rpm"
     var hrefs = html.match(/href="[^\"]+"/g);
     for (var i = 0; i < hrefs.length; i++) {
-        href = hrefs[i];
-        url = href.split('"')[1];
-        parts = url.split('.');
-        console.debug(url, parts);
+        var href = hrefs[i];
+        var url = href.split('"')[1];
+        if url.match(/\.manifest\.xml$/) {
+            var parts = url.split('.');
+            console.debug(url, parts);
+        }
     }
 }
 
