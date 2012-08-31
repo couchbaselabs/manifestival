9208f27f0f880626893dc588804c9b47c1a37f46
Steve Yen <steve.yen@gmail.com>
2012-08-30 18:10:20 -0700

    added gen() func

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function on_index_html(html) {
     // Look for patterns like:  href="couchbase-server-community_x86_64_2.0.0c-709-rel.rpm.manifest.xml"
 
     var urls = {}
-    var data = {} // Hierarchy of maps, keyed by name / version / build / arch / pkg.
+    var hier = {} // Hierarchy of maps, keyed by name / version / build / arch / pkg.
 
     var hrefs = html.match(/href="[^\"]+\.manifest\.xml"/g);
     for (var i = 0; i < hrefs.length; i++) {
@@ -26,13 +26,11 @@ function on_index_html(html) {
                       "pkg": pkg };
             urls[url] = p;
 
-            add_entry(data, [name, version, build, arch, pkg], p);
+            add_entry(hier, [name, version, build, arch, pkg], p);
         }
     }
 
-    console.debug($('#content'));
-    console.debug(urls);
-    console.debug(data);
+    $('#content').html(gen(hier));
 }
 
 function add_entry(hier, path, val) {
@@ -43,7 +41,38 @@ function add_entry(hier, path, val) {
         }
         hier = m;
     }
-    hier[path.length - 1] = val;
+    hier[path[path.length - 1]] = val;
+}
+
+function gen(hier) {
+    var r = [];
+    r[r.length] = '<ul>';
+    for (var name in hier) {
+        r[r.length] = '<li>' + name + '<ul>';
+        var versions = hier[name];
+        for (var version in versions) {
+            r[r.length] = '<li>' + version + '<ul>';
+            var builds = versions[version];
+            for (var build in builds) {
+                r[r.length] = '<li>' + build + '<ul>';
+                var archs = builds[build];
+                for (var arch in archs) {
+                    r[r.length] = '<li>' + arch;
+                    var pkgs = archs[arch];
+                    for (var pkg in pkgs) {
+                        var p = pkgs[pkg];
+                        r[r.length] = '<a href="' + p.url + '">' + pkg + '</a>';
+                    }
+                    r[r.length] = '</li>';
+                }
+                r[r.length] = '</ul></li>';
+            }
+            r[r.length] = '</ul></li>';
+        }
+        r[r.length] = '</ul></li>';
+    }
+    r[r.length] = '</ul>';
+    return r;
 }
 
 function on_ready() {
