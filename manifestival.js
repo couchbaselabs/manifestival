629ef2d77336016ed3532080d08d58035e82ac67
Steve Yen <steve.yen@gmail.com>
2015-02-19 18:39:12 -0800

    loadProduct/Release/Build

--- manifestival.js
+++ manifestival.js
@@ -1,37 +1,71 @@
+var products = [
+  "couchbase-server"
+];
+
 console.log("manifestival awakes");
 
 var scriptServer = "127.0.0.1:8000"
-var scriptsWanted = [];
-var scriptsReady = [];
+var scripts = [
+    "jquery-2.1.3.min.js",
+    "js-yaml-3.2.6.min.js",
+    "underscore-1.7.0.min.js",
+    "velocity-1.2.2/velocity.min.js",
+    "velocity-1.2.2/velocity.ui.min.js"
+];
 
-function addScript(s) {
-    scriptsWanted.push(s);
-    var e = document.getElementById("script-" + s);
-    if (!e) {
-        e = document.createElement("script");
-        e.id = "script-" + s;
-        e.src = "http://" + scriptServer + "/js/" + s;
-        function scriptReady() {
-            scriptsReady.push(s);
-            if (scriptsReady.length >= scriptsWanted.length) {
-                main();
-            }
-        }
-        if (e.addEventListener) {
-            e.addEventListener("load", scriptReady, false);
-        } else if (e.readyState) {
-            e.onreadystatechange = scriptReady;
-        }
-        document.body.appendChild(e);
+function loadScripts() {
+    var s = scripts.shift();
+    if (!s) {
+        return main();
+    }
+    var e = document.createElement("script");
+    e.id = "script-" + s;
+    e.src = "http://" + scriptServer + "/js/" + s;
+    if (e.addEventListener) {
+        e.addEventListener("load", loadScripts, false);
+    } else if (e.readyState) {
+        e.onreadystatechange = loadScripts;
+    }
+    document.body.appendChild(e);
+}
+
+loadScripts();
+
+// ---------------------------------------------------------------
+
+function main() {
+    console.log("manifestival main");
+    for (var i = 0; i < products.length; i++) {
+        loadProduct(products[i]);
     }
 }
 
-addScript("jquery-2.1.3.min.js");
-addScript("js-yaml-3.2.6.min.js");
-addScript("underscore-1.7.0.min.js");
-addScript("velocity-1.2.2/velocity.min.js");
-addScript("velocity-1.2.2/velocity.ui.min.js");
+function loadProduct(product) { // Ex: "couchbase-server".
+    console.log("loadProduct", product);
+    $.ajax("/" + product, {
+        success: function(h) {
+            var hrefs = h.match(/href="[a-z][a-z\-_0-9]+\/"/g);
+            for (var i = 0; hrefs && i < hrefs.length; i++) {
+                var release = hrefs[i].split('"')[1].replace('/', ''); // Ex: "sherlock".
+                loadProductRelease(product, release);
+            }
+        }
+    });
+}
 
-function main() {
-    console.log("manifestival is ready to PARTY!");
+function loadProductRelease(product, release) {
+    console.log("loadProductRelease", product, release);
+    $.ajax("/" + product + "/" + release, {
+        success: function(h) {
+            var hrefs = h.match(/href="[0-9][0-9]+\/"/g);
+            for (var i = 0; hrefs && i < hrefs.length; i++) {
+                var build = hrefs[i].split('"')[1].replace('/', ''); // Ex: "1129".
+                loadProductReleaseBuild(product, release, build);
+            }
+        }
+    });
+}
+
+function loadProductReleaseBuild(product, release, build) {
+    console.log("loadProductReleaseBuild", product, release, build);
 }
