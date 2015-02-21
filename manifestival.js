fb4b996023a4c0f409879308d17a378d8f01deac
Steve Yen <steve.yen@gmail.com>
2015-02-20 17:48:09 -0800

    whitespace

--- manifestival.js
+++ manifestival.js
@@ -212,7 +212,6 @@ function loadProductReleaseBuildArtifact(product, release, build, artifact) {
         return;
     }
 
-
     addFacet("arch", arch);
     addFacet("edition", edition);
     addFacet("ext", ext);
